263c3b607e615960777e515bdd9a91510604cbc0
Marc Poppy <poppy.marc@gmail.com>
2019-08-05 01:07:03 +0000

    added menu method to CLI

--- lib/crypto/command_line_interface.rb
+++ lib/crypto/command_line_interface.rb
@@ -4,6 +4,11 @@
 class CLI
 
   def call
+    list_crypto
+    menu
+  end
+
+  def list_crypto
     puts "Today's crypto prices."
     puts <<-DOC
       1. Bitcoin BTC $10K
@@ -13,9 +18,18 @@ class CLI
     DOC
   end
 
-  # def list_crypto
-  # end
-  #
-  # def menu
-  # end
+  def menu
+    puts "Enter the number for which you would like to get more inforation on."
+    input = gets.strip
+    case input
+    when "1"
+      puts "more information on crypto 1"
+    when "2"
+      puts "more information on crypto 2"
+    when "3"
+      puts "more information on crypto 3"
+    when "4"
+      puts "more information on crypto 4"
+    end
+  end
 end
