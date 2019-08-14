790ba34ab2493ddf722bfdd717163eee901d67cc
Marc Poppy <poppy.marc@gmail.com>
2019-08-14 03:58:29 +0000

    updated scraper class

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,12 +9,16 @@ class Scraper
   end
 
   def price_scraper
+      cryptos = []
       crypto_page = self.get_page.css("tbody.AssetTable__AssetTableBody-sc-3hlimn-0 tr")
-      crypto_page.each do |crypto|
-            name = crypto.search("h4.Header__StyledHeader-sc-1q6y56a-0")[1].text
-            symbol = crypto.search("h4.Header__StyledHeader-sc-1q6y56a-0")[2].text
-            puts "#{name}: #{symbol}"
-      end
+      crypto_page.each_with_index do |crypto, index|
+          name = crypto.search("h4.Header__StyledHeader-sc-1q6y56a-0")[1].text unless index == 16
+          symbol = crypto.search("h4.Header__StyledHeader-sc-1q6y56a-0")[2].text unless index == 16
+          puts "#{index + 1}.#{name}: #{symbol}"
+          cryptos << {name: name, symbol: symbol} unless index == 16
+        end
+        binding.pry
+        cryptos
     end
 
   def assign_crypto
