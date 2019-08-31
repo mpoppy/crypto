21ea3b534b7f11607d6cd54e66248cbf507d7d19
Marc Poppy <poppy.marc@gmail.com>
2019-08-31 00:27:46 +0000

    CLI update

--- lib/crypto/command_line_interface.rb
+++ lib/crypto/command_line_interface.rb
@@ -6,19 +6,27 @@ class CLI
   def call
     puts "Today's Top Cryptocurrencies:"
     list_crypto
-    menu
+    puts "Would you like to get more information on a specific cryptocurrency? Y/N"
+    input = gets.strip.downcase
+    if input == "yes" || input == "y"
+      add_attributes_to_crypto
+      menu
+    else
+      breakd
+    end
   end
 
   def list_crypto
     crypto_array = Scraper.new.price_scraper
     Currency.create_from_collection(crypto_array)
     Currency.all.each_with_index do |c, i|
-      puts "#{i + 1}. #{c.name} - #{c.symbol} price:#{c.price}"
+      puts "#{i + 1}. #{c.name} - #{c.symbol} - price:#{c.price}"
       sleep 0.25
     end
   end
 
   def add_attributes_to_crypto
+    puts "Downloading Details, one moment.."
     Currency.all.each do |currency|
       attributes = Scraper.new.detail_scraper(currency.url)
       currency.add_crypto_attributes(attributes)
@@ -31,8 +39,6 @@ class CLI
     #to relist cryptos type list
     #dont allow numb
     input = gets.strip.downcase
-    puts "Downloading Details, one moment.."
-    add_attributes_to_crypto
     if input != "exit"
       input = input.to_i - 1
       Currency.all.select.with_index do |c, i|
